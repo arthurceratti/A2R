e0d15c58d1a7acabbead8be0f16c89c177486796
Arthur Ceratti <arthurceratti@gmail.com>
2020-08-25 13:53:52 -0300

    deploy version

--- mysite/register/forms.py
+++ mysite/register/forms.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .models import Empresa #?
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm #formulário necessário para criar um novo usuário
 from django.contrib.auth.models import User
@@ -7,5 +8,6 @@ class RegisterForm(UserCreationForm):
     email = models.EmailField()
 
     class Meta:
+
         model = User
         fields = ["username", "email", "password1", "password2"]
